51f724f7f3b9023e238e9cb7e41b0222e36066bd
Jackson Harris III <jackson@spotx.games>
2023-01-30 13:16:10 -0500

    update nav bar

--- Components/Navigation.tsx
+++ Components/Navigation.tsx
@@ -11,6 +11,19 @@ import {
   GridItem,
   Heading,
   Spinner,
+  Avatar,
+  Flex,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+  Card,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 
@@ -18,6 +31,7 @@ import Link from 'next/link';
 import { Web3Button } from '@web3modal/react';
 import { useWeb3AuthHook } from '../utils/web3AuthContext';
 import { useAccount } from 'wagmi';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { useContext, useEffect } from 'react';
 
 const Navigation = () => {
@@ -26,6 +40,7 @@ const Navigation = () => {
     { href: '/ar-page', name: 'AR Page' },
     { href: '/account-info', name: 'Account Info' },
   ];
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { address, isConnecting, isDisconnected } = useAccount();
   const {
     w3aAddress,
@@ -38,46 +53,78 @@ const Navigation = () => {
   useEffect(() => {}, [web3authProvider]);
 
   return (
-    <Grid templateColumns="repeat(1fr 3fr 1fr)" gap={2} p={5}>
-      <GridItem>
-        <Heading size="xl">AR Web3 Starter</Heading>
-      </GridItem>
-
-      <GridItem justifySelf="start">
-        <Container>
-          <Grid templateColumns="repeat(5, 1fr)" gap={2} pt={3}>
-            {pages.map((page, index) => (
-              <Link href={page.href} key={index}>
-                <GridItem>
-                  <Text as="i">{page.name}</Text>
-                </GridItem>
-              </Link>
-            ))}
-          </Grid>
-        </Container>
-      </GridItem>
-
-      <GridItem justifySelf="end" colStart={5} p={2} gap={1}>
-        {w3aAddress ? (
-          <Button onClick={web3AuthLogout}>Logout</Button>
-        ) : (
-          <>
-            <Web3Button />
-            {/* If we're logged in with wallet connect we don't want to show the Torus login button */}
-            {address ? null : (
-              <Button ml={1} mb={2} onClick={web3authLogin}>
-                Web2 Sign-In
-              </Button>
+    <>
+      <Box
+        // bg={useColorModeValue('purple.500', 'purple.400')}
+        px={3}
+        py={2}
+        boxShadow="xl"
+      >
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+            mr={2}
+            bg={useColorModeValue('blue.400', 'blue.400')}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>
+              <Heading size="m">AR Web3 Starter</Heading>
+            </Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}
+            >
+              {pages.map((page, index) => (
+                <Link href={page.href} key={index}>
+                  <GridItem>
+                    <Text as="i">{page.name}</Text>
+                  </GridItem>
+                </Link>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            {w3aAddress ? (
+              <Button onClick={web3AuthLogout}>Logout</Button>
+            ) : (
+              <>
+                <Web3Button />
+                {/* If we're logged in with wallet connect we don't want to show the Torus login button */}
+                {address ? null : (
+                  <Button ml={1} onClick={web3authLogin}>
+                    Web2 Sign-In
+                  </Button>
+                )}
+              </>
             )}
-          </>
-        )}
-        {gettingAccount ? (
-          <>
-            <Spinner />
-          </>
+            {gettingAccount ? (
+              <>
+                <Spinner />
+              </>
+            ) : null}
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {pages.map((page, index) => (
+                <Link href={page.href} key={index}>
+                  <GridItem>
+                    <Text as="i">{page.name}</Text>
+                  </GridItem>
+                </Link>
+              ))}
+            </Stack>
+          </Box>
         ) : null}
-      </GridItem>
-    </Grid>
+      </Box>
+    </>
   );
 };
 
