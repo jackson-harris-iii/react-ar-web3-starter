2a652c31bd78c34a6628a82671a0fd1b401b7075
Jackson Harris III <jackson@spotx.games>
2023-03-05 00:53:56 -0500

    add web3 button to launcher

--- Components/Launcher.tsx
+++ Components/Launcher.tsx
@@ -1,6 +1,7 @@
 import Layout from './Layout';
 import { Network, Alchemy } from 'alchemy-sdk';
 import { Card, Center, Container } from '@chakra-ui/react';
+import { Web3Button } from '@web3modal/react';
 
 const Launcher = ({ nfts }) => {
   return (
@@ -14,7 +15,7 @@ const Launcher = ({ nfts }) => {
                   dangerouslySetInnerHTML={{
                     __html: `<iframe
             id="game"
-            style="border: 0; width: 35vh; height: 50vh"
+            style="border: 0; width: 35vh; height: 75vh"
             allow="camera;gyroscope;accelerometer;magnetometer;xr-spatial-tracking;microphone;geolocation;"
             src="https://nftstorage.link/ipfs/${nft.rawMetadata.animation_url.replace(
               'ipfs://',
@@ -27,6 +28,7 @@ const Launcher = ({ nfts }) => {
             </Card>
           ))
         : 'nothing to launch'}
+      <Web3Button />
     </Container>
   );
 };
